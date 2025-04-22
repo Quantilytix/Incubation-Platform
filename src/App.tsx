ab2391833c22a77579dcb3c5220cea9612bfb869
Daniel-Rumona <danielrumona@gmail.com>
2025-04-22 11:24:51 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,23 @@
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-
-import { RefineThemes, useNotificationProvider } from "@refinedev/antd";
-import { Authenticated, ErrorComponent, Refine } from "@refinedev/core";
-import { DevtoolsProvider } from "@refinedev/devtools";
-import routerProvider, {
-  CatchAllNavigate,
-  DocumentTitleHandler,
-  NavigateToResource,
-  UnsavedChangesNotifier,
-} from "@refinedev/react-router-v6";
-
-import { App as AntdApp, ConfigProvider } from "antd";
-
-import { Layout } from "@/components";
-import { resources } from "@/config/resources";
-import { authProvider, dataProvider, liveProvider } from "@/providers";
-import { DashboardPage, LoginPage, RegisterPage } from "@/routes";
-
-import "@refinedev/antd/dist/reset.css";
+import { Refine } from "@refinedev/core";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import routerProvider from "@refinedev/react-router-v6";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <ConfigProvider theme={RefineThemes.Blue}>
-        <AntdApp>
-          <DevtoolsProvider>
-            <Refine
-              routerProvider={routerProvider}
-              dataProvider={dataProvider}
-              liveProvider={liveProvider}
-              notificationProvider={useNotificationProvider()}
-              authProvider={authProvider}
-              resources={resources}
-              options={{
-                syncWithLocation: true,
-                warnWhenUnsavedChanges: true,
-                liveMode: "auto",
-                useNewQueryKeys: true,
-              }}
-            >
-              <Routes>
-                <Route
-                  element={
-                    <Authenticated
-                      key='authenticated-layout'
-                      fallback={<CatchAllNavigate to='/login' />}
-                    >
-                      <Layout>
-                        <Outlet />
-                      </Layout>
-                    </Authenticated>
-                  }
-                >
-                  <Route index element={<DashboardPage />} />
-
-                  <Route path='*' element={<ErrorComponent />} />
-                </Route>
-
-                <Route
-                  element={
-                    <Authenticated
-                      key='authenticated-auth'
-                      fallback={<Outlet />}
-                    >
-                      <NavigateToResource resource='dashboard' />
-                    </Authenticated>
-                  }
-                >
-                  <Route path='/login' element={<LoginPage />} />
-                </Route>
-                <Route path='/register' element={<RegisterPage />} />
-              </Routes>
-              <UnsavedChangesNotifier />
-              <DocumentTitleHandler />
-            </Refine>
-          </DevtoolsProvider>
-        </AntdApp>
-      </ConfigProvider>
+      <Refine
+        routerProvider={routerProvider}
+        options={{
+          syncWithLocation: true,
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<h1 style={{ padding: "3rem" }}>Refine is rendering!</h1>} />
+        </Routes>
+      </Refine>
     </BrowserRouter>
   );
 };
 
 export default App;
+
